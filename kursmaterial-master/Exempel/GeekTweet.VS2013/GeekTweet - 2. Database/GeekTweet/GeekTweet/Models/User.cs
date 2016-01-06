aa941d41bdf39deb50b017446697c23a4577d008
henceee <hg222dv@student.lnu.se>
2016-01-06 16:05:02 +0100

    Forgot to sync last night, fml.
    Here's the first change, examples from 1vd409 lectures. Hopefully github
    doesn't freak out bc of 4400+ changes...........

---
+++ kursmaterial-master/Exempel/GeekTweet.VS2013/GeekTweet - 2. Database/GeekTweet/GeekTweet/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace GeekTweet.Models
+{
+    public partial class User
+    {
+        public User(JToken userToken)
+            : this()
+        {
+            Id = (string)userToken["id_str"];
+            Name = (string)userToken["name"];
+            ScreenName = (string)userToken["screen_name"];
+        }
+    }
+}
